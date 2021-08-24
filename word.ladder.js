42c32bd33caade9b5e7f5166ac19c791c3e3e1bb
Ravi Sahu <sahuravi@users.noreply.github.com>
2021-08-24 19:47:51 +0530

    Update word.ladder.js
    Updating comments.

--- word.ladder.js
+++ word.ladder.js
@@ -1,6 +1,8 @@
 /**
  * Question 1: Word Ladder (Length of shortest chain to reach a target word)
- * Given a dictionary, and two words ‘start’ and ‘target’ (both of same length). Find length of the smallest chain from ‘start’ to ‘target’ if it exists, such that adjacent words in the chain only differ by one character and each word in the chain is a valid word i.e., it exists in the dictionary. It may be assumed that the ‘target’ word exists in dictionary and length of all dictionary words is same.
+ * Given a dictionary, and two words ‘start’ and ‘target’ (both of same length). Find length of the smallest chain from ‘start’ to ‘target’ if it exists, such that adjacent words 
+ * in the chain only differ by one character and each word in the chain is a valid word i.e., it exists in the dictionary. It may be assumed that the ‘target’ word exists in 
+ * dictionary and length of all dictionary words is same.
  * Exp: Input:  Dictionary = {POON, PLEE, SAME, POIE, PLEA, PLIE, POIN}
  * start = TOON
  * target = PLEA
